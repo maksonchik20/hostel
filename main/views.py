98e7b4ff23b2a2ea8ea775f27d68d82a741a58c7
maksonchik20 <elena-g0903@mail.ru>
2023-03-10 16:55:39 +0300

    templates and views render

--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Client
 
 
 def index(request):
-    return HttpResponse('test')
+    # здесь можно получить данные из бд и передать их также в data
+    data = {
+        'text': 'some text from python views.py',
+        'title':'Клиенты',
+        'clients': Client.objects.all()
+        }
+    return render(request, 'main/index.html', data)
 
 def some_page(request):
     return HttpResponse('hello')
