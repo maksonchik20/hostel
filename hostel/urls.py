25ce1fa00bd016c55ad9fd9e437fdff4f5821f66
necromfox <necromfox@gmail.com>
2023-03-15 10:47:16 +0300

    Сайт админа называется нормальо:

--- hostel/urls.py
+++ hostel/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.index_title = "Администрирование Turbis-Dev"
+admin.site.site_title = "Администрирование Turbis-Dev"
+admin.site.site_header = "Администрирование Turbis-Dev"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
